48dcb573ee85abc471247139c20e12ae3092e655
Aria Fatah <ariafatah0711@gmail.com>
2025-02-12 20:17:41 +0700

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://ariaf.my.id/docs",
+  url: "https://ariaf.my.id",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
